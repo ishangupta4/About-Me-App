6911b8cd921bf0d97cfc0b2697c042d353b289c3
Ishan Gupta <ishangupta004@gmail.com>
2020-01-21 00:29:47 +0530

    Add files via upload

---
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputBinding
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val myName = MyName("Ishan Gupta")
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        //findViewById<Button>(R.id.done_button).setOnClickListener{
+          //  addNickName(it)
+        //}
+        binding.doneButton.setOnClickListener{
+            addNickName(it)
+        }
+
+    }
+
+    private fun addNickName(view: View) {
+        /*val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE*/
+
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            binding.nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ AboutMe/app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package com.example.aboutme
+
+import android.provider.ContactsContract
+
+data class MyName(var name: String = "", var nickname: String = "")
+
